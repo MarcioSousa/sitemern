db21fd883cb8ceec6b55da4ade3b74f28bb694ff
Marcio Sousa <marciofrapeso@gmail.com>
2020-10-22 22:14:17 -0300

    Modelo pronto para aulas de Web (Mern Stack) do Professor Ricardo Leme, usando material-ui, react. botões aparecendo na tela

---
+++ src/App.js
@@ -0,0 +1,41 @@
+//cSpell:Ignore MEUS, temaDark, tipoPaleta, corPrimaria, corSecundaria, Teste
+import React from 'react'
+import './App.css'
+import { createMuiTheme, responsiveFontSizes, MuiThemeProvider} from '@material-ui/core'
+
+/* cores */
+import { orange, lightBlue, blue, deepOrange} from '@material-ui/core/colors'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Button from '@material-ui/core/Button'
+
+/* MEUS IMPORTS */
+//import Rotas from './rotas'
+
+//import Button from '@material-ui/core/Button'
+
+export default function App(){
+    const temaDark = false
+    const tipoPaleta = temaDark ? 'dark' : 'light'
+    const corPrimaria = temaDark ? orange[500] : blue[500]
+    const corSecundaria = temaDark ? deepOrange[900] : lightBlue[400]
+
+    let theme = createMuiTheme(
+        {
+            palette : {
+                type: tipoPaleta,
+                primary: { main: corPrimaria},
+                secondary: {main: corSecundaria}
+            }
+        }
+    )
+
+    theme = responsiveFontSizes(theme)
+
+    return (
+        <MuiThemeProvider theme={theme}> 
+            <CssBaseline/>
+            <Button variant="contained" color="primary">Teste</Button>
+            <Button variant="contained" color="secondary">Teste 2</Button>
+        </MuiThemeProvider>
+    )
+}
